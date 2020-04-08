bf7be7f0eedfa81e999b841ba74f09462a7483df
mfindlater <michael.findlater+gh@gmail.com>
2020-04-08 18:07:01 +0900

    Add check before appending to $PATH.

--- setup/add_profile.bash
+++ setup/add_profile.bash
@@ -3,6 +3,13 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 SCRIPT_NAME=`basename "$0"`
 
+path_is_installed() {
+    [[ -n "$(echo ${PATH} | grep 'profs/bin')" ]]
+}
+
 virtualenv -p `which python3` venv
 ${SCRIPT_DIR}/venv/bin/pip3 install -r ${SCRIPT_DIR}/requirements.txt
+
+# Add to `$PATH` if not there already.
+path_is_installed && exit 0
 echo "export PATH="${SCRIPT_DIR}/bin:\$PATH"" >> ${HOME}/.profile
